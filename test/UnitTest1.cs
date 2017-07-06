e05051e7e33f9e46d1714b8eedc318c5c5e9a58d
Ian Keough <ian.keough@autodesk.com>
2017-07-05 20:59:12 -0700

    Include some examples.

--- test/UnitTest1.cs
+++ test/UnitTest1.cs
@@ -1,14 +1,24 @@
 using System;
+using System.IO;
+using System.Xml.Serialization;
 using Xunit;
 using IFC4;
 
 namespace test
 {
-    public class UnitTest1
-    {
-        [Fact]
-        public void Test1()
-        {
-        }
-    }
+	public class UnitTest1
+	{
+		[Fact]
+		public void CreateWall()
+		{
+			var building = new IfcBuilding();
+			building.Name = "Ifc Test Building";
+			
+			var project = new IfcProject();
+			project.Name = "Ifc Test Project";
+			project.UnitsInContext = new IfcUnitAssignment();
+			project.UnitsInContext.Units = new IfcUnitAssignmentUnits();
+
+		}
+	}
 }
