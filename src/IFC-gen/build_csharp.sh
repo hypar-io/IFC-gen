553c1c771f42a05b07c26537af7047c0f51489c3
Ian Keough <ian.keough@autodesk.com>
2017-07-24 16:27:47 -0700

    Move IFC-dotnet to separate project.

--- src/IFC-gen/build_csharp.sh
+++ src/IFC-gen/build_csharp.sh
@@ -2,4 +2,4 @@
 antlr4 -Dlanguage=CSharp -package Express -o ./csharp/antlr Express.g4
 dotnet build ./csharp/IFC-gen.csproj
 dotnet run -p ./csharp/IFC-gen.csproj IFC4.exp ../IFC-dotnet
-dotnet build ../IFC-dotnet/IFC-dotnet.csproj
+dotnet build ../../../IFC-dotnet/IFC-dotnet.csproj
