c43aa8da58382a74f876c3c7475f6f84d8025ca5
Ian Keough <ian.keough@autodesk.com>
2017-07-24 16:33:37 -0700

    Fix output directory.

--- src/IFC-gen/build_csharp.sh
+++ src/IFC-gen/build_csharp.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 antlr4 -Dlanguage=CSharp -package Express -o ./csharp/antlr Express.g4
 dotnet build ./csharp/IFC-gen.csproj
-dotnet run -p ./csharp/IFC-gen.csproj IFC4.exp ../IFC-dotnet
-dotnet build ../../../IFC-dotnet/IFC-dotnet.csproj
+dotnet run -p ./csharp/IFC-gen.csproj IFC4.exp ../../../IFC-dotnet/src/IFC-dotnet
