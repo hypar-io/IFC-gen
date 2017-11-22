92c66a76cbfa6e5a6736c1d7d8ee858e521a8982
Simon Moreau <simon@bim42.com>
2017-11-22 22:48:51 +0100

    Edit make.bat

--- make.bat
+++ make.bat
@@ -1,17 +1,17 @@
 SET ANTLR=java -jar C:\Javalib\antlr-4.7-complete.jar
 SET CURR_DIR=%cd%
-SET GRAMMAR_IFC=%CURR_DIR%/Express.g4
-SET GRAMMAR_STEP=%CURR_DIR%/STEP.g4
-SET SCHEMA=%CURR_DIR%/IFC4.exp
+SET GRAMMAR_IFC=%CURR_DIR%\Express.g4
+SET GRAMMAR_STEP=%CURR_DIR%\STEP.g4
+SET SCHEMA=%CURR_DIR%\IFC4.exp
 
 ::csharp:
-	%ANTLR% -Dlanguage=CSharp -package Express -o ./src/antlr %GRAMMAR_IFC%
-	%ANTLR% -Dlanguage=CSharp -package STEP -o ./lang/csharp/src/antlr %GRAMMAR_STEP%
+	%ANTLR% -Dlanguage=CSharp -package Express -o %CURR_DIR%\src\antlr %GRAMMAR_IFC%
+	%ANTLR% -Dlanguage=CSharp -package STEP -o %CURR_DIR%\lang\csharp\src\antlr %GRAMMAR_STEP%
 	dotnet build .\src\IFC-gen.csproj
-	dotnet run -p .\src\IFC-gen.csproj %SCHEMA% ./lang/csharp/src
+	dotnet run -p .\src\IFC-gen.csproj %SCHEMA% .\lang\csharp\src
 
 ::clean:
-	rmdir /s /q .\lang\csharp\src\antlr
+	::rmdir /s /q .\lang\csharp\src\antlr
 	rmdir /s /q .\src\antlr
 	rmdir /s /q .\src\bin
 	rmdir /s /q .\src\obj
