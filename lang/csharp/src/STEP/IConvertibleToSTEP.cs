5687d8baaad49bbffc641f03052b9bfd4c8d2c2b
Ian Keough <ian@iankeough.com>
2017-12-18 22:02:40 -0800

    Erase a bogus comment.

--- lang/csharp/src/STEP/IConvertibleToSTEP.cs
+++ lang/csharp/src/STEP/IConvertibleToSTEP.cs
@@ -2,17 +2,6 @@ namespace STEP
 {
     public interface IConvertibleToSTEP
     {
-        /// <summary>
-        /// Is this object an option of a SELECT? 
-        /// This can be used in the case of IFC, where
-        /// SELECT instances used as parameter in a
-        /// constructor, are expected to be of the form
-        /// IFCBOOLEAN(.T.). Setting isSelectOption=true
-        /// will convey this to the location where the 
-        /// value is encoded.
-        /// </summary>
-        /// <param name="isSelectOption"></param>
-        /// <returns></returns>
         string ToStepValue(bool isSelectOption = false);
     }
 }
