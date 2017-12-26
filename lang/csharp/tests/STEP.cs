9b9c282c6c39fc027a28a120dc559021dc59d82f
Ian Keough <ian@iankeough.com>
2017-12-25 23:53:20 -0500

    Assert model instance counts, where possible.

--- lang/csharp/tests/STEP.cs
+++ lang/csharp/tests/STEP.cs
@@ -21,24 +21,41 @@ namespace IFC4.Tests
         }
 
         [Fact]
-        public void ExampleModel_Deserialize_STEP()
+        public void ExampleModel_Deserialize()
         {
             var stepPath = "../../../models/example.ifc";
             IList<STEPError> errors;
             var model = new Model(stepPath, new LocalStorageProvider(), out errors);
-            var project = model.AllInstancesOfType<IfcProject>().FirstOrDefault();
             ReportErrors(stepPath, errors);
+            Assert.Equal(283, model.AllInstances.Count());
         }
 
         [Fact]
-        public void ExampleModel_Serialize_STEP()
+        public void ExampleModel_Serialize()
         {
+            // Load the example model
             var stepPath = "../../../models/example.ifc";
             IList<STEPError> errors;
             var model = new Model(stepPath, new LocalStorageProvider(), out errors);
+            Assert.Equal(283, model.AllInstances.Count());
+            
+            // Serialize the example model
             var outputPath = "../../../models/output.ifc";
             File.WriteAllText(outputPath, model.ToSTEP(outputPath));
             ReportErrors(stepPath, errors);
+
+            // Reload the new version of the model
+            var newModel = new Model(outputPath, new LocalStorageProvider(), out errors);
+            Assert.Equal(283, newModel.AllInstances.Count());
+        }
+
+        [Fact]
+        public void ScientificNotation_Deserialize()
+        {
+            var stepPath = "../../../models/scientific_notation.ifc";
+            IList<STEPError> errors;
+            var model = new Model(stepPath, new LocalStorageProvider(), out errors);
+            Assert.Equal(4, model.AllInstances.Count());
         }
 
         [Fact]
@@ -47,33 +64,37 @@ namespace IFC4.Tests
             var stepPath = "../../../models/AC-20-Smiley-West-10-Bldg.ifc";
             IList<STEPError> errors;
             var model = new Model(stepPath, new LocalStorageProvider(), out errors);
+            //Assert.Equal(205693, model.AllInstances.Count());
             ReportErrors(stepPath, errors);
         }
 
         [Fact]
-        public void Hospital_Deserialize_STEP()
+        public void Hospital_Deserialize()
         {
             var stepPath = "../../../models/20160125WestRiverSide Hospital - IFC4-Autodesk_Hospital_Sprinkle.ifc";
             IList<STEPError> errors;
             var model = new Model(stepPath, new LocalStorageProvider(), out errors);
+            //Assert.Equal(952819, model.AllInstances.Count());
             ReportErrors(stepPath, errors);
         }
 
         [Fact]
-        public void PropertySet_Deserialize_STEP()
+        public void PropertySet_Deserialize()
         {
-            var stepPath = "../../../models/propertySet.ifc";
+            var stepPath = "../../../models/property_set.ifc";
             IList<STEPError> errors;
             var model = new Model(stepPath, new LocalStorageProvider(), out errors);
+            Assert.Equal(11, model.AllInstances.Count());
             ReportErrors(stepPath, errors);
         }
 
         [Fact]
-        public void CanDeserializeStepWithSelect()
+        public void Select_Deserialize()
         {
             var stepPath = "../../../models/select.ifc";
             IList<STEPError> errors;
             var model = new Model(stepPath, new LocalStorageProvider(), out errors);
+            Assert.Equal(4, model.AllInstances.Count());
             ReportErrors(stepPath, errors);
         }
 
