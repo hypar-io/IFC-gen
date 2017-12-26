82000d88bc338dab36d181823156d6a17474c8b2
Ian Keough <ian@iankeough.com>
2017-12-25 22:03:57 -0500

    Fix formatting once.

--- src/Program.cs
+++ src/Program.cs
@@ -11,132 +11,142 @@ using NDesk.Options;
 
 namespace IFC.Generate
 {
-	class Program
-	{
-		private static string language;
-		private static string outDir;
-		private static string outDirTests;
-		private static string expressPath;
-		private static bool showHelp;
-		private static bool outputTokens;
+    class Program
+    {
+        private static string language;
+        private static string outDir;
+        private static string outDirTests;
+        private static string expressPath;
+        private static bool showHelp;
+        private static bool outputTokens;
 
-		static void Main(string[] args)
-		{
-			ParseOptions(args);
-			if(showHelp){
-				return;
-			}
+        static void Main(string[] args)
+        {
+            ParseOptions(args);
+            if (showHelp)
+            {
+                return;
+            }
 
-			var generators = new List<Tuple<ILanguageGenerator, ITestGenerator, IFunctionsGenerator>>();
+            var generators = new List<Tuple<ILanguageGenerator, ITestGenerator, IFunctionsGenerator>>();
 
-			if(language == "csharp"){
-				generators.Add(new Tuple<ILanguageGenerator, ITestGenerator, IFunctionsGenerator>(
-					new CsharpLanguageGenerator(), new CsharpTestGenerator(), new CsharpFunctionsGenerator()));
-			}else if(language == "proto"){
-				generators.Add(new Tuple<ILanguageGenerator, ITestGenerator, IFunctionsGenerator>(new ProtobufGenerator(), new ProtobufTestGenerator(), null));
-			}
+            if (language == "csharp")
+            {
+                generators.Add(new Tuple<ILanguageGenerator, ITestGenerator, IFunctionsGenerator>(
+                    new CsharpLanguageGenerator(), new CsharpTestGenerator(), new CsharpFunctionsGenerator()));
+            }
+            else if (language == "proto")
+            {
+                generators.Add(new Tuple<ILanguageGenerator, ITestGenerator, IFunctionsGenerator>(new ProtobufGenerator(), new ProtobufTestGenerator(), null));
+            }
 
-			using (FileStream fs = new FileStream(expressPath, FileMode.Open))
-			{
-				var input = new AntlrInputStream(fs);
-				var lexer = new Express.ExpressLexer(input);
-				var tokens = new CommonTokenStream(lexer);
+            using (FileStream fs = new FileStream(expressPath, FileMode.Open))
+            {
+                var input = new AntlrInputStream(fs);
+                var lexer = new Express.ExpressLexer(input);
+                var tokens = new CommonTokenStream(lexer);
 
-				var parser = new Express.ExpressParser(tokens);
-				parser.BuildParseTree = true;
+                var parser = new Express.ExpressParser(tokens);
+                parser.BuildParseTree = true;
 
-				var tree = parser.schemaDecl();
-				var walker = new ParseTreeWalker();
-				
-				var testSb = new StringBuilder();
+                var tree = parser.schemaDecl();
+                var walker = new ParseTreeWalker();
 
-				foreach(var generator in generators){
-					var listener = new Express.ExpressListener(generator.Item1, generator.Item2);
-					walker.Walk(listener, tree);
-					Generate(listener, outDir, outDirTests, generator.Item1, generator.Item2, generator.Item3);
-				}
-				
-				if(!outputTokens)
-				{
-					return;
-				}
-				
-				var tokenStr = new StringBuilder();
-				foreach(var t in tokens.GetTokens())
-				{
-					tokenStr.AppendLine(t.ToString());
-				}
-				Console.WriteLine(tokenStr);
-			}
-		}
+                var testSb = new StringBuilder();
 
-		private static void Generate(Express.ExpressListener listener, string outDir, string outDirTests, 
-		ILanguageGenerator generator, ITestGenerator testGenerator, IFunctionsGenerator functionsGenerator){
-			var codePath = Path.Combine(outDir, generator.FileName);
-			var testPath = Path.Combine(outDirTests, testGenerator.FileName);
+                foreach (var generator in generators)
+                {
+                    var listener = new Express.ExpressListener(generator.Item1, generator.Item2);
+                    walker.Walk(listener, tree);
+                    Generate(listener, outDir, outDirTests, generator.Item1, generator.Item2, generator.Item3);
+                }
 
-			var codeSb = new StringBuilder();
-			var testSb = new StringBuilder();
+                if (!outputTokens)
+                {
+                    return;
+                }
 
-			codeSb.AppendLine(generator.Begin());
-			testSb.AppendLine(testGenerator.Begin());
-			foreach(var kvp in listener.TypeData)
-			{
-				var td = kvp.Value;
-				codeSb.AppendLine(td.ToString());
+                var tokenStr = new StringBuilder();
+                foreach (var t in tokens.GetTokens())
+                {
+                    tokenStr.AppendLine(t.ToString());
+                }
+                Console.WriteLine(tokenStr);
+            }
+        }
 
-				// Only write tests for entities.
-				var entity = td as Express.Entity;
+        private static void Generate(Express.ExpressListener listener, string outDir, string outDirTests,
+        ILanguageGenerator generator, ITestGenerator testGenerator, IFunctionsGenerator functionsGenerator)
+        {
+            var codePath = Path.Combine(outDir, generator.FileName);
+            var testPath = Path.Combine(outDirTests, testGenerator.FileName);
+
+            var codeSb = new StringBuilder();
+            var testSb = new StringBuilder();
+
+            codeSb.AppendLine(generator.Begin());
+            //testSb.AppendLine(testGenerator.Begin());
+            foreach (var kvp in listener.TypeData)
+            {
+                var td = kvp.Value;
+                codeSb.Append(td.ToString());
+
+                // Only write tests for entities.
+                /*var entity = td as Express.Entity;
 				if(entity != null){
 					testSb.AppendLine(entity.ToTestString());
-				}
-			}
-			codeSb.AppendLine(generator.End());
-			testSb.AppendLine(testGenerator.End());
+				}*/
+            }
+            codeSb.AppendLine(generator.End());
+            //testSb.AppendLine(testGenerator.End());
 
-			File.WriteAllText(codePath,codeSb.ToString());
-			File.WriteAllText(testPath,testSb.ToString());
+            File.WriteAllText(codePath, codeSb.ToString());
+            //File.WriteAllText(testPath,testSb.ToString());
 
-			if(functionsGenerator != null)
-			{	
-				var functionsPath = Path.Combine(outDir, functionsGenerator.FileName);
-				File.WriteAllText(functionsPath, functionsGenerator.Generate(listener.FunctionData.Values));
-			}
-		}
+            if (functionsGenerator != null)
+            {
+                var functionsPath = Path.Combine(outDir, functionsGenerator.FileName);
+                File.WriteAllText(functionsPath, functionsGenerator.Generate(listener.FunctionData.Values));
+            }
+        }
 
-		private static void ParseOptions(string[] args){
-			var p = new OptionSet () {
-				{ "e|express=", "The path the express schema.", v => expressPath = v },
-				{ "o|output=", "The directory in which the code is generated.", v => outDir = v},
-				{ "t|test=", "The directory in which the test code is generated.", v => outDirTests = v},
-				{ "l|language=", "The target language (csharp)", v => language = v},
-				{ "p|tokens", "Output tokens to stdout during parsing.", v => outputTokens = v != null},
-				{ "h|help",   v => showHelp = v != null },
-			};
-			
-			List<string> extra;
-			try {
-				extra = p.Parse (args);
-			}
-			catch (OptionException e) {
-				Console.Write ("IFC-gen: ");
-				Console.WriteLine (e.Message);
-				Console.WriteLine ("Try `IFC-gen --help' for more information.");
-				return;
-			}
+        private static void ParseOptions(string[] args)
+        {
+            var p = new OptionSet() {
+                { "e|express=", "The path the express schema.", v => expressPath = v },
+                { "o|output=", "The directory in which the code is generated.", v => outDir = v},
+                { "t|test=", "The directory in which the test code is generated.", v => outDirTests = v},
+                { "l|language=", "The target language (csharp)", v => language = v},
+                { "p|tokens", "Output tokens to stdout during parsing.", v => outputTokens = v != null},
+                { "h|help",   v => showHelp = v != null },
+            };
 
-			if(showHelp){
-				ShowHelp(p);
-				return;
-			}
-		}
+            List<string> extra;
+            try
+            {
+                extra = p.Parse(args);
+            }
+            catch (OptionException e)
+            {
+                Console.Write("IFC-gen: ");
+                Console.WriteLine(e.Message);
+                Console.WriteLine("Try `IFC-gen --help' for more information.");
+                return;
+            }
 
-		private static void ShowHelp (OptionSet p)
-		{
-			Console.WriteLine ("Usage: IFC-gen [OPTIONS]+");
-			Console.WriteLine ();
-			Console.WriteLine ("Options:");
-			p.WriteOptionDescriptions (Console.Out);
-		}
-	}
+            if (showHelp)
+            {
+                ShowHelp(p);
+                return;
+            }
+        }
+
+        private static void ShowHelp(OptionSet p)
+        {
+            Console.WriteLine("Usage: IFC-gen [OPTIONS]+");
+            Console.WriteLine();
+            Console.WriteLine("Options:");
+            p.WriteOptionDescriptions(Console.Out);
+        }
+    }
 }
