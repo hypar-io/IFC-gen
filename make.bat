3522a198e7a7944af3707779130d256c84b3d2a1
Gytaco <sheatherbim@gmail.com>
2018-03-04 21:17:50 +1000

    Antlr clean moved to not remove ExpressBaseListener from dotnet references.

--- make.bat
+++ make.bat
@@ -1,10 +1,11 @@
-SET ANTLR=java -jar C:\Javalib\antlr-4.7-complete.jar
+SET ANTLR=java -jar C:\Javalib\antlr-4.7.1-complete.jar
 SET CURR_DIR=%cd%
 SET GRAMMAR_IFC=%CURR_DIR%\Express.g4
 SET GRAMMAR_STEP=%CURR_DIR%\STEP.g4
 SET SCHEMA=%CURR_DIR%\IFC4.exp
 
 ::csharp:
+    rmdir /s /q .\src\antlr
 	%ANTLR% -Dlanguage=CSharp -package Express -o %CURR_DIR%\src\antlr %GRAMMAR_IFC%
 	%ANTLR% -Dlanguage=CSharp -package STEP -o %CURR_DIR%\lang\csharp\src\antlr %GRAMMAR_STEP%
 	dotnet build .\src\IFC-gen.csproj
@@ -12,6 +13,5 @@ SET SCHEMA=%CURR_DIR%\IFC4.exp
 
 ::clean:
 	rmdir /s /q .\lang\csharp\src\antlr
-	rmdir /s /q .\src\antlr
 	rmdir /s /q .\src\bin
 	rmdir /s /q .\src\obj
